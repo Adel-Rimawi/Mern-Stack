dc35beca758026157fa31c2f58f3cbab3201c50a
Adel-Rimawi <a.s.riimawi@gmail.com>
2021-07-18 00:51:48 +0300

    first_jsx

---
+++ React/first_jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo</h1>
+      <h3>Things i need to do</h3>
+      <ul>
+        <li>Do 1</li>
+        <li>Do 2</li>
+        <li>Do 3</li>
+        <li>Do 4</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
