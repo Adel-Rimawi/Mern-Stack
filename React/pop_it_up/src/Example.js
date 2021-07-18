ba5e9a40b4ad197f7cd2ebb2b1959396154ce116
Adel-Rimawi <a.s.riimawi@gmail.com>
2021-07-18 08:20:30 +0300

    putting it together

--- React/pop_it_up/src/Example.js
+++ React/pop_it_up/src/Example.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 
 export class Example extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {Age: this.props.age};
+    }
+    const 
+    
+    
     render() {
         return (
             <div>
                 <h1>{this.props.lastname} , {this.props.firstname}</h1>
-                <p>Age : {this.props.age}</p>
+                <p>Age : {this.state.Age}</p>
                 <p>Hair Color : {this.props.haircolor}</p>
+                { <button  onClick={ () => { this.setState ({ Age: this.state.Age +1 })  }}  >Add </button> }
             </div>
         )
     }
