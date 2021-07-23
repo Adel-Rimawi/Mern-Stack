6b4ed06cb6a879267c2d24885b265b5f0ebc7df4
Adel-Rimawi <a.s.riimawi@gmail.com>
2021-07-24 00:46:16 +0300

    hook form

---
+++ React/hook_form/src/App.js
@@ -0,0 +1,12 @@
+import Userform from './userForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Userform></Userform>
+    </div>
+  );
+}
+
+export default App;
