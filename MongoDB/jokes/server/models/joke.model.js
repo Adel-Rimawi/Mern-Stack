36ad44c3b0bea9c2cc9425ad5230a9f0c86ddc98
Adel-Rimawi <a.s.riimawi@gmail.com>
2021-07-30 23:20:19 +0300

    jokes API

---
+++ MongoDB/jokes/server/models/joke.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const JokeSchema = new mongoose.Schema({
+	setup: String,
+	punchline: String,
+	createdAt:{ type: Date, default: Date.now }
+});
+
+const Joke = mongoose.model("joke", JokeSchema);
+
+module.exports = Joke;
